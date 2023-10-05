fba1abc2d43429c8637a43c49d3e059067b5814e
gunturrr <gunturwisnu72@gmail.com>
2023-10-06 00:42:49 +0800

    baru

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -50,7 +50,7 @@ const Main = () => {
                 alt={movie?.title}
               />
               <div className="absolute w-full top-[35%] p-4 md:p-8">
-                <h1 className="text-4xl md:text-7xl font-bold text-red-600 w-1/2">
+                <h1 className="text-4xl md:text-6xl font-bold text-red-600">
                   {movie?.title}
                 </h1>
                 
